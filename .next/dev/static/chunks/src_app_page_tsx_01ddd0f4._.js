7ab3faf14b0bf03770551927f3efbd18eb8eaa21
kkcreator1-collab <kkcreator1@gmail.com>
2026-03-22 14:53:47 +0530

    update

---
+++ .next/dev/static/chunks/src_app_page_tsx_01ddd0f4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_83e1a48e._.js",
+  "static/chunks/node_modules_20dd58c5._.js"
+],
+    source: "dynamic"
+});

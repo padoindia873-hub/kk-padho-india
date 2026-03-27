02b4ea6f2b540cf1f52bcf81a8fe5032bb633685
padoindia873-hub <padoindia873@gmail.com>
2026-03-27 22:09:03 +0530

    updated

--- .next/dev/server/chunks/ssr/[root-of-the-server]__994d7c77._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__994d7c77._.js
@@ -523,15 +523,6 @@ function Footer() {
                                         fileName: "[project]/src/components/Footer.tsx",
                                         lineNumber: 151,
                                         columnNumber: 15
-                                    }, this),
-                                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
-                                        href: "/faq",
-                                        className: "text-gray-400 hover:text-blue-400 transition-colors",
-                                        children: "FAQ"
-                                    }, void 0, false, {
-                                        fileName: "[project]/src/components/Footer.tsx",
-                                        lineNumber: 152,
-                                        columnNumber: 15
                                     }, this)
                                 ]
                             }, void 0, true, {
--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -216,6 +216,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/member/join/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/member/join">> = Specific
+  const handler = {} as typeof import("../../../src/app/member/join/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../src/app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
